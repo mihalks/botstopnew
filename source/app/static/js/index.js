5d4961ee6e6d1f61b3bbc85ce364a0230019cc71
mihalks <43777460+mihalks@users.noreply.github.com>
2021-12-10 08:17:52 +0300

    Delete source/app directory

--- source/app/static/js/index.js
+++
@@ -1,5 +0,0 @@
-const canvasEl = document.querySelector('#canvas');
-const editorBl = document.querySelector('#editor');
-const startFrm = document.querySelector('#startForm');
-
-const app = new App(startFrm, canvasEl, editorBl);
